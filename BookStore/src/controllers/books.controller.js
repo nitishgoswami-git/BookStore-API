5dc945d2cf5ed866679914af3b4a2311c86a5267
Nitish Goswami <itsgoswaminitish@gmail.com>
2025-04-10 06:46:06 +0000

    bug fixes and testing

--- BookStore/src/controllers/books.controller.js
+++ BookStore/src/controllers/books.controller.js
@@ -2,6 +2,7 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {Book} from "../models/book.model.js"
+import { isValidObjectId } from "mongoose";
 
 const getAllBooks = asyncHandler(async (req,res) =>{
     // query Book db for all books
@@ -9,7 +10,7 @@ const getAllBooks = asyncHandler(async (req,res) =>{
     // else throw error
     try{
         const BookList = await Book.find({})
-        if(BookList?.length<0){
+        if(BookList?.length === 0){
             return res.status(200)
             .json(
                 new ApiResponse(
@@ -41,13 +42,14 @@ const getSingleBookById = asyncHandler(async (req,res)=>{
      // find book based on id
      // if found send res
      // else throw error
-
-     const {bookId} = req.params
+     console.log(req.params);
+     const bookId = req.params.id 
+     console.log(bookId)
      if(!isValidObjectId(bookId)){
         throw new ApiError(400,"BookId not valid")
      }
 
-    const bookDetails = await Book.findOne({_id:bookId})
+    const bookDetails = await Book.findById(bookId)
     if(!bookDetails){
             throw new ApiError(400,"Book Not Found")
     }
@@ -69,7 +71,11 @@ const addNewBook = asyncHandler(async(req,res)=>{
     // check if book exists
     // create entry
     // send response
+    if (!req.body) {
+        throw new ApiError(400, "Request body is missing");
+      }
     const {title,author,year} = req.body
+    console.log(title,author,year)
     if (
         [title,author,year].some((field)=>
         field?.trim() === "")
@@ -78,7 +84,7 @@ const addNewBook = asyncHandler(async(req,res)=>{
     }
 
     const bookExists = await Book.findOne({"title":title, "author":author})
-    if(!bookExists){
+    if(bookExists){
         throw new ApiError(400,"Book Already Exists")
     }
     const bookData = await Book.create({
@@ -102,6 +108,76 @@ const addNewBook = asyncHandler(async(req,res)=>{
     )
 })
 
+const updateBook = asyncHandler (async(req,res)=>{
+    // get id
+    // get newData
+    // verify id
+    // find book
+    // update book
+    // return res
+
+    const bookId = req.params.id
+    const {title,author,year} = req.body
+
+    if(!isValidObjectId(bookId)){
+        throw new ApiError(400,"Not a valid Id")
+    }
+
+    if (
+        [title,author,year].some((field)=>
+        field?.trim() === "")
+    ){
+        throw new ApiError(400, " All Fields are required")
+    }
+
+    const book = await Book.findById(bookId)
+    if(!book){
+        throw new ApiError(400,"Book not found")
+    }
+    const updatedBook = await Book.findByIdAndUpdate(bookId,{
+        title,
+        author,
+        year
+    },{new:true})
+
+    return res.status(200)
+    .json(
+        new ApiResponse(
+            200,
+            updatedBook,
+            "update Successful"
+
+        )
+    )
+})
+
+const deleteBook = asyncHandler (async(req,res)=>{
+    // get book Id
+    // verify bookId
+    // find book
+    // delete
+    // return res
+
+    const bookId = req.params.id
+    if(!isValidObjectId(bookId)){
+        throw new ApiError(400,"Not a valid Id")
+    }
+
+    const book = await Book.findById(bookId)
+    if(!book){
+        throw new ApiError(404,"Book Not found")
+    }
+
+    await Book.findByIdAndDelete(bookId)
+    return res.status(200)
+    .json(
+        new ApiResponse(
+            200,
+            {},
+            "Book deleted Successfully"
+        )
+    )
+})
 export {
   getAllBooks,
   getSingleBookById,
